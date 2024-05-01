edf3e895207ae43247266932fd6f5ad24ad79d66
Pedro Françozo <pedro3francozo@gmail.com>
2024-04-30 21:31:53 -0300

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,5 @@
+async function fetcgProfileData() {
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
