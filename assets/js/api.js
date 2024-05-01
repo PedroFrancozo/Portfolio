fd2c8623a90abf1cdd602ce7c776ea0e089135fb
Pedro Françozo <pedro3francozo@gmail.com>
2024-05-01 08:32:54 -0300

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetcgProfileData() {
     const url = 'https://github.com/PedroFrancozo/Portfolio/blob/main/assets/data/profile.json';
     const fetching = await fetch(url)
+    const profileData = await response.json()
     return await fetching.json()
 }
